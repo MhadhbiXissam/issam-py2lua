2e02e213fc1c168b030785ee54d844e9cb14dde8
Mhadhbi Issam <95243282+MhadhbiXissam@users.noreply.github.com>
2023-01-24 15:32:38 +0100

    Update new_pylua_file.nim

--- PythonicLua/new_pylua_file.nim
+++ PythonicLua/new_pylua_file.nim
@@ -15,9 +15,9 @@ cwd = os.getcwd()
 new_file_path = "." + path + ".pylua"
 default_code = ""
 if path.endswith(".script") : 
-    default_code = "def init(self) : \n\tpass\n\n\ndef final(self) : \n\tpass\n\n\ndef update(self, dt) : \n\tpass\n\n\ndef fixed_update(self, dt) : \n\tpass\n\n\ndef on_message(self, message_id, message, sender)\n\tpass\n\n\ndef on_input(self, action_id, action) : \n\tpass\n\n\ndef on_reload(self)\n\tpass"
+    default_code = "def init(self) : \n\tpass\n\n\ndef final(self) : \n\tpass\n\n\ndef update(self, dt) : \n\tpass\n\n\ndef fixed_update(self, dt) : \n\tpass\n\n\ndef on_message(self, message_id, message, sender) : \n\tpass\n\n\ndef on_input(self, action_id, action) : \n\tpass\n\n\ndef on_reload(self) : \n\tpass\n"
 if path.endswith(".lua") : 
-    default_code = "M : local = {}\n\n# private\nmessage : local = \"Hello world!\"\n\ndef M.hello()\n\tprint(message)\n\n\nreturn M"
+    default_code = "M : local = {}\n\n# private\nmessage : local = \"Hello world!\"\n\ndef M.hello() : \n\tprint(message)\n\n\nreturn M"
 
 if not os.path.exists(new_file_path) : 
     print(default_code, file = open(new_file_path,"w"))
